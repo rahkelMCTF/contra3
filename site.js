828d12e6901b55e525f5cde2b593cff161141cfc
rahkel <49926743+rahkelMCTF@users.noreply.github.com>
2020-05-18 14:15:18 -0300

    random equipe

--- site.js
+++ site.js
@@ -2,16 +2,18 @@ var tema = [];
 var i = 0;
 var sorteados = [];
 //var valorMaximo = 16;
+var equipes = [];
 window.onload = function() {
    tema = ["Pseudo-código","POO", "Teste Caixa-Preta", "Teste Caixa-Branca", "Java", "C++", "C", "Testes Funcionais", 
    "Teste de Não Funcionais", "Validação e Verificação", "Cenário de Teste", 
    "Paradigmas de Programação", "Diagrama de Classe", "Operadores lógicos", "Leitura e Interpretação", 
    "Estrutura de Dados", "Engenharia de Requisitos", "Diagrama de Atividades", "Diagramas de Casos de Uso", 
    "Diagramas de Classes", "Ciclos de vida de Processos"];
+   equipes = ["Equipe1", "Equipe2", "Equipe3", "Equipe4", "Equipe5"];
 }
 exibir = function(id)
 {
-    i = criarUnico();
+    i = criarUnico(tema.length);
     document.getElementById(id).innerHTML = "<h2>" + tema[i] + "</h2>";
     document.getElementById(id).classList.remove('cartao');  
     document.getElementById(id).classList.add('texto');  
@@ -33,10 +35,17 @@ exibir = function(id)
    // i++;
 }
 
-function criarUnico() {
+sortearEquipe = function(){
+    var e = criarUnico(equipes.length);
+    document.getElementById("equipe").classList.remove('cartao');  
+    document.getElementById("equipe").classList.add('texto');  
+    document.getElementById("equipe").innerHTML= "<h1>" + equipes[e] + "</h1>";
+}
+
+function criarUnico(tam) {
     var sorteados = [];
-    var valorMaximo = tema.length-1;
-    if (sorteados.length == tema.length) {
+    var valorMaximo = tam-1;
+    if (sorteados.length == tam) {
         if (confirm('Já não há mais! Quer recomeçar?')) sorteados = [];
         else return;
     }
